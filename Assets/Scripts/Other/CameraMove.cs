5025102ab0959495d2b8d673ff8fd4dd2ba9383d
pufferi <148252886+pufferi@users.noreply.github.com>
2025-12-17 22:40:25 +0800

    处理字体和intro

---
+++ Assets/Scripts/Other/CameraMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private InputActionAsset inputActions;
+    private InputAction moveAction;
+
+
+
+    void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+
+    void Awake()
+    {
+        moveAction = inputActions.FindActionMap("Player").FindAction("Move");
+    }
+
+    void Update()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        if (input.sqrMagnitude > 0f)
+        {
+            Vector3 move = Vector3.right * input.x + Vector3.forward * input.y;
+            transform.position += move * moveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Other/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ChangeScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+   public void ChangeSceneto(int idx)
+    {
+        SceneManager.LoadScene(idx);
+    }
+}
